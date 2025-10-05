b868a698295ea566cbd9efdd0824c39bfa7526ae
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-10-05 11:52:43 +0200

    feat: added models to store type specific information in database

---
+++ core_site/homework_org_site/models.py
@@ -0,0 +1,77 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import ManyToManyField
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="teacher",
+    )
+    bio = models.CharField(max_length=200)
+    subjects = ManyToManyField('Subject')
+
+    def __str__(self):
+        return self.user.username
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="student",
+    )
+    school_year = models.IntegerField()
+    notes = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    student = models.ForeignKey(
+        'Student',
+        on_delete=models.CASCADE,
+        related_name='lessons'
+    )
+    teacher = models.ForeignKey(
+        'Teacher',
+        on_delete=models.CASCADE,
+        related_name='lessons'
+    )
+    subject = models.ForeignKey(
+        'Subject',
+        on_delete=models.CASCADE,
+        related_name='lessons'
+    )
+    date = models.DateField()
+    duration = models.IntegerField(default=45)
+    topic = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.subject.name + ' ' + self.teacher.user.username
+
+class Homework(models.Model):
+    student = models.ForeignKey(
+        'Student',
+            on_delete=models.CASCADE,
+            related_name='homeworks'
+    )
+    subject = models.ForeignKey(
+        'Subject',
+            on_delete=models.CASCADE,
+            related_name='homeworks'
+    )
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+
+
