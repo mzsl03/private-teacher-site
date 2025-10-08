9a2532a41d7c17688c4ff4e6081bc34f821e7ab7
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-10-08 11:53:26 +0200

    feat: new page to edit homework title, description and due date

---
+++ core_site/homework_org_site/forms/homework_update_form.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from homework_org_site.models import Homework
+
+class HomeworkUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Homework
+        fields = ['title', 'description', 'due_date']
+        widgets = {
+            "description": forms.Textarea(attrs={"rows": 3}),
+            "due_date": forms.DateInput(attrs={"type": "date"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].label = "Title"
+        self.fields["description"].label = "Description"
+        self.fields["due_date"].label = "Due date"
