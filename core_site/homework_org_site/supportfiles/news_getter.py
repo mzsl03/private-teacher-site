eaf8884481fd635718ace8c8cfbcdc147fbb9422
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-10-12 13:31:47 +0200

    fix: unicode chars are unescaped

--- core_site/homework_org_site/supportfiles/news_getter.py
+++ core_site/homework_org_site/supportfiles/news_getter.py
@@ -1,5 +1,6 @@
 import feedparser
 from bs4 import BeautifulSoup as bf
+import html
 
 def news_getter() -> tuple[list, list, list]:
     feed = feedparser.parse('https://www.theverge.com/rss/index.xml')
@@ -9,7 +10,8 @@ def news_getter() -> tuple[list, list, list]:
     for entry in feed.entries[:5]:
         content = entry.content[0].value
         soup = bf(content, 'html.parser')
-        titles.append(entry.title)
+        titles.append(html.unescape(entry.title))
         links.append(entry.link)
-        contents.append(soup.get_text(strip=True))
+        contents.append(html.unescape(soup.get_text(strip=True)))
+
     return titles, links, contents
